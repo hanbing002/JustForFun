9c16beb355788207cc2502d2536104cbf095bea4
hanbing <haing_2323@126.com>
2019-03-19 14:58:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/justforfun/DisplayMessageActivity.java
@@ -0,0 +1,33 @@
+package com.example.justforfun;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ConstraintLayout layout = findViewById(R.id.content);
+        layout.addView(textView);
+    }
+
+}
